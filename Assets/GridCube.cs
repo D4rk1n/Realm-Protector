452507f51e42adb8ac43d569f6ac1d7ab1ad9ce8
Darkin <darkinb255@gmail.com>
2019-06-23 10:01:14 +0200

    Snap to Grid

---
+++ Assets/GridCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GridCube : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+        float x = Mathf.RoundToInt(transform.position.x / 10) * 10;
+        float z = Mathf.RoundToInt(transform.position.z / 10) * 10;
+        transform.position = new Vector3(x, 0, z);
+    }
+}
