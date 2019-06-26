fa7724649239ca4ad87220796008a0b0f7baf7e4
Darkin <darkinb255@gmail.com>
2019-06-26 17:10:33 +0200

    Explore Ns

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +8,42 @@ public class PathFinder : MonoBehaviour
     [SerializeField] Waypoint StartPoint = null;
     [SerializeField] Waypoint EndPoint = null;
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] Dir =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
 
     // Start is called before the first frame update
     void Start()
     {
+        CreateGrid();
+        ExploreNeighbours(StartPoint);
+    }
+
+    private void ExploreNeighbours(Waypoint waypoint)
+    {
+        
+        foreach (Vector2Int direction in Dir)
+        {
+            Vector2Int neighbour = waypoint.GetPosition() + direction;
+            print(neighbour);
+           if(grid.ContainsKey(neighbour))
+            grid[neighbour].SetTopColor(Color.cyan);
+          
+                
+            
+        }
+    }
+
+    private void CreateGrid()
+    {
         Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
         foreach (Waypoint waypoint in waypoints)
         {
-            var gridPos = waypoint.GetPosition() * 10;
+            var gridPos = waypoint.GetPosition() ;
             if (grid.ContainsKey(gridPos))
             {
                 Debug.LogWarning("Waypoint " + waypoint.name + " is Overlapping");
@@ -25,8 +54,8 @@ public class PathFinder : MonoBehaviour
                 grid.Add(gridPos, waypoint);
             }
         }
-        StartPoint.SetTopColor(Color.green);
-        EndPoint.SetTopColor(Color.cyan);
+        StartPoint.SetTopColor(Color.blue);
+        EndPoint.SetTopColor(Color.green);
     }
 
     // Update is called once per frame
