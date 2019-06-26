87523a6ac5027fc98a4843564bdaf7249eef9e0d
Darkin <darkinb255@gmail.com>
2019-06-26 23:05:09 +0200

    Enemy Movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public PathFinder pathFinder = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        pathFinder = FindObjectOfType<PathFinder>();
+        var path = pathFinder.GetPath();
+        StartCoroutine(FollowPath(path));
+
+        // print("Enemy " +path.Count);
+    }
+    IEnumerator FollowPath(Stack<Waypoint> path)
+    {
+        
+        while (path.Count > 0)
+        {
+            Waypoint wp = path.Pop();
+           // print(wp);
+            transform.position = wp.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
