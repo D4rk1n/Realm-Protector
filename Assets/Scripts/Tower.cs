4d59df5ab5cac42b625902869ac7f46a81e4093f
Darkin <darkinb255@gmail.com>
2019-06-27 13:46:09 +0200

    Tower

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform TowerHead;
+    [SerializeField] Transform Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TowerHead.LookAt(Enemy);
+    }
+}
