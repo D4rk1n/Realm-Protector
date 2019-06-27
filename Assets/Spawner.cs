01e4ecd1fbb00a78794ea5120a9baa9110188f42
Darkin <darkinb255@gmail.com>
2019-06-27 18:15:05 +0200

    spawner added

---
+++ Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject Enemy = null;
+    [SerializeField] float SpawnTime = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        print("Spawn");
+        yield return new WaitForSeconds(SpawnTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
