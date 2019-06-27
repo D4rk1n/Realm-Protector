cacf7ed66af5bf44eed952411b19011b188c3083
Darkin <darkinb255@gmail.com>
2019-06-27 23:25:06 +0200

    Enemy OnHitFX

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int Health = 5 ;
+    [SerializeField] ParticleSystem OnHitFX = null;
+    [SerializeField] ParticleSystem DeathFX = null;
 
     // Update is called once per frame
     void Update()
     {
+
         if (Health <= 0)
+        {
+            Instantiate(DeathFX, new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z), Quaternion.identity);
             Destroy(gameObject);
+        }
     }
     private void OnParticleCollision(GameObject other)
     {
-       
         Health--;
+        OnHitFX.Play();
     }
     private void OnTriggerEnter(Collider other)
     {
